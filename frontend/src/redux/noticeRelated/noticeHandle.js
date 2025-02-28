5fcf0e8bb0b6ea68feda9ee7f44561cd87ead838
sikdarriton <31799010+sikdarriton@users.noreply.github.com>
2025-02-28 16:37:32 +0530

    Update noticeHandle.js

--- frontend/src/redux/noticeRelated/noticeHandle.js
+++ frontend/src/redux/noticeRelated/noticeHandle.js
@@ -6,11 +6,13 @@ import {
     getError
 } from './noticeSlice';
 
+const REACT_APP_BASE_URL = "https://education-system-backend.onrender.com";
+
 export const getAllNotices = (id, address) => async (dispatch) => {
     dispatch(getRequest());
 
     try {
-        const result = await axios.get(`${process.env.REACT_APP_BASE_URL}/${address}List/${id}`);
+        const result = await axios.get(`${REACT_APP_BASE_URL}/${address}List/${id}`);
         if (result.data.message) {
             dispatch(getFailed(result.data.message));
         } else {
