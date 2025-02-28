f9cdad4641ce7f65129565a1d322b119cc3531a8
sikdarriton <31799010+sikdarriton@users.noreply.github.com>
2025-02-28 16:39:05 +0530

    Update userHandle.js

--- frontend/src/redux/userRelated/userHandle.js
+++ frontend/src/redux/userRelated/userHandle.js
@@ -14,6 +14,7 @@ import {
 } from './userSlice';
 
 const REACT_APP_BASE_URL = "https://education-system-backend.onrender.com";
+
 export const loginUser = (fields, role) => async (dispatch) => {
     dispatch(authRequest());
 
