b3f3e43a1340dd6c776e8945e8045b5b078bb3a0
sikdarriton <31799010+sikdarriton@users.noreply.github.com>
2025-02-28 14:21:42 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -20,6 +20,14 @@ dotenv.config();
 app.use(express.json({ limit: '10mb' }))
 app.use(cors())
 const MONGO_URL = "mongodb+srv://sikdarriton:gqWICTDo33KTxFjy@cluster-education-syste.ntqoz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster-Education-System";
+
+mongoose.connection.on('connected', () => console.log('connected'));
+mongoose.connection.on('open', () => console.log('open'));
+mongoose.connection.on('disconnected', () => console.log('disconnected'));
+mongoose.connection.on('reconnected', () => console.log('reconnected'));
+mongoose.connection.on('disconnecting', () => console.log('disconnecting'));
+mongoose.connection.on('close', () => console.log('close'));
+
 mongoose
     .connect(MONGO_URL, {
         useNewUrlParser: true,
@@ -27,6 +35,8 @@ mongoose
     })
     .then(console.log("Connected to MongoDB"))
     .catch((err) => console.log("NOT CONNECTED TO NETWORK", err))
+
+
 
 app.use('/', Routes);
 
